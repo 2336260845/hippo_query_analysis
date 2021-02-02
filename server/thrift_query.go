755af05182cf83797a32e7e74323a3b5a096bb71
mayongzhen.hippo <mayongzhen.hippo@bytedance.com>
2021-02-02 16:25:05 +0800

    feat(gomod): 更新gomod

--- server/thrift_query.go
+++ server/thrift_query.go
@@ -3,15 +3,15 @@ package server
 import (
 	"context"
 	"fmt"
+	"github.com/2336260845/hippo_search/gen-go/query_analysis"
 	"github.com/apache/thrift/lib/go/thrift"
 	"hippo_query_analysis/config"
 	"hippo_query_analysis/es"
-	"hippo_query_analysis/gen-go/query_analysis"
 )
 
 type QueryAnalysisServer struct{}
 
-func (qas *QueryAnalysisServer) QueryAnalysis(ctx context.Context, req *query_analysis.QueryParam) (r []string, err error) {
+func (qas *QueryAnalysisServer) QueryAnalysis(_ context.Context, req *query_analysis.QueryParam) (r []string, err error) {
 	if req.Query == "" {
 		return []string{}, fmt.Errorf("query is empty")
 	}
